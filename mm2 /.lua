b8f7764e7fa87278bf5d9d279442cefdcc770b9e
nzea243 <vladneza6@gmail.com>
2026-02-25 12:17:53 +0300

    Add script loading functionality with SCRIPT_KEY

---
+++ mm2 /.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/f0a44ff31c7b0cf609e8ece0679f4e451b3bea3d65a4822741537c76238c023d/download"))()
